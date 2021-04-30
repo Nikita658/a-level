5eaa628aa38834649203f135022385ba8b67a804
Nikita658 <nikita14bykov@gmail.com>
2021-04-30 20:43:31 +0300

    My first commit

---
+++ src/PrintsNameAndDate.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class PrintsNameAndDate {
+    public static void main(String[] args) {
+        System.out.println("Nikita Bykov");
+        System.out.print(new Date());
+    }
+}
