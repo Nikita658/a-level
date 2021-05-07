7667e42ae553c5e45a0c35c38904eda96831cf10
Nikita658 <nikita14bykov@gmail.com>
2021-05-07 19:06:02 +0300

    Task2 checking words in a string

--- src/ua/Bykov/HW3/Task2.java
+++ src/ua/Bykov/HW3/Task2.java
@@ -7,15 +7,19 @@ public class Task2 {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter words separated by space");
         String input = s.nextLine();
+        System.out.println(countWord(input));
+    }
+
+    private static int countWord(String input) {
         int count = 0;
-        if(input.length() != 0){
+        if (input.length() != 0) {
             count++;
             for (int i = 0; i < input.length(); i++) {
-                if(input.charAt(i) == ' '){
+                if (input.charAt(i) == ' ' && input.charAt(i - 1) != ' ') {
                     count++;
                 }
             }
         }
-        System.out.println("In this line "+count+"In this line");
+        return count;
     }
 }
