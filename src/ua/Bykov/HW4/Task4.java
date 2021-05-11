973628059dcbdfa3139d854def51373ec12ddd36
Nikita658 <nikita14bykov@gmail.com>
2021-05-11 19:18:55 +0300

    replacing even values with zeros

--- src/ua/Bykov/HW4/Task4.java
+++ src/ua/Bykov/HW4/Task4.java
@@ -1,24 +1,27 @@
 package ua.Bykov.HW4;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Task4 {
     public static void main(String[] args) {
         int array[] = new int[2000];
         int[] fillTheArray = write(array);
+        System.out.println( Arrays.toString(fillTheArray));
         System.out.println("even numbers are replaced " + changeEvenToZero(fillTheArray));
     }
 
     private static int changeEvenToZero(int[] array) {
         int count = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0) ;
+            if (array[i] % 2 == 0)
             {
                 array[i] = 0;
                 count++;
             }
         }
+        System.out.println(Arrays.toString(array));
         return count;
     }
 
